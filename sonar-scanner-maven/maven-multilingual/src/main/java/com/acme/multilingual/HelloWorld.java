3bb131fa3e0baa96afba85dca391d27551bd6471
admin <yj.seong@curvc.com>
2025-03-19 11:11:41 +0900

    tetest

--- sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
+++ sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
@@ -1,8 +1,12 @@
+/**
+ * The {@code HelloWorld} class provides methods to print messages to the console.
+ * It includes a method to print a greeting message and another method that is not covered by unit tests.
+ */
 package com.acme.multilingual;
 
 public class HelloWorld {
 
-  void sayHello() {
+  public void sayHello() {
     System.out.println("Hello World!");
   }
 
