590176032167f6561eebfce62cf383682222e9ce
admin <yj.seong@curvc.com>
2025-03-20 10:34:01 +0900

    test

--- sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
+++ sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/HelloWorld.java
@@ -1,21 +1,21 @@
-/**
- * The {@code HelloWorld} class provides methods to print messages to the console.
- * It includes a method to print a greeting message and another method that is not covered by unit tests.
- */
 package com.acme.multilingual;
-
-import java.util.logging.Logger;
 
 public class HelloWorld {
 
-  private static final Logger LOGGER = Logger.getLogger(HelloWorld.class.getName());
-
-  public void sayHello() {
-    LOGGER.info("Hello World!");
+  void sayHello() {
+    System.out.println("Hello World!");
+    System.out.println("Welcome to the multilingual application!");
   }
 
-  void notCovered() {
-    LOGGER.info("This method is not covered by unit tests");
+  public static void main(String[] args) {
+    HelloWorld helloWorld = new HelloWorld();
+    helloWorld.sayHello();
   }
+
+    void notCovered() {
+    System.out.println("This method is not covered by unit tests.");
+  }
+
+  
 
 }
