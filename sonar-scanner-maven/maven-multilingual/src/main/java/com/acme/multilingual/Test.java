fe72de2b0ef0d95788bc349b62ad8c2cbf3b7f2e
admin <yj.seong@curvc.com>
2025-03-20 10:20:45 +0900

    test

--- sonar-scanner-maven/maven-multilingual/src/main/java/com/acme/multilingual/Test.java
+++
@@ -1,9 +0,0 @@
-package com.acme.multilingual;
-
-// This code was generated with the assistance of GitHub Copilot.
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Welcome to the multilingual project.");
-    }
-}
